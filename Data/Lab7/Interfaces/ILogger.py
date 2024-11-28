0fe8bc0c1b9726cbd053ae708c139fa35a02580e
pablo3737 <arbuzenko09@gmail.com>
2024-11-28 22:16:22 +0200

    Initial commit

---
+++ Data/Lab7/Interfaces/ILogger.py
@@ -0,0 +1,31 @@
+# Interfaces/ILogger.py
+# abstract patern
+
+from abc import ABC, abstractmethod
+
+class ILogger(ABC):
+    @abstractmethod
+    def log_query(self, query, result):
+        pass
+
+# Concrete implementations
+class FileLogger(ILogger):
+    def log_query(self, query, result):
+        with open("log.txt", "a") as file:
+            file.write(f"Query: {query}, Result: {result}\n")
+
+class ConsoleLogger(ILogger):
+    def log_query(self, query, result):
+        print(f"Query: {query}, Result: {result}")
+
+# Usage with dependency injection
+class QueryProcessor:
+    def __init__(self, logger: ILogger):
+        self.logger = logger
+
+    def process_query(self, query):
+        # Process the query and get the result (dummy result here for example)
+        result = f"Result for {query}"
+        self.logger.log_query(query, result)
+        return result
+
---
+++ Data/Lab8/Constants/config.py
@@ -0,0 +1,3 @@
+# Constants/config.py
+CSV_FILE_PATH = 'Data/Lab8/Assets/your_file.csv'
+IMAGE_FILE_PATH = 'Data/Lab8/Assets/sales_amount_interactive.html'
